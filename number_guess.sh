3c11c68dd582880eb9a185041d5d0fce0983d870
Ma. Agustina  Benitez Vanni <magustinabenitez@gmail.com>
2024-11-12 14:45:28 +0000

    feat: final logic

--- number_guess.sh
+++ number_guess.sh
@@ -21,3 +21,28 @@ SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 GUESSES=0
 GUESS=0
 
+echo "Guess the secret number between 1 and 1000:"
+
+while [[ $GUESS -ne $SECRET_NUMBER ]]
+do
+  read GUESS
+  
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  else
+    ((GUESSES++)) 
+
+    if [[ $GUESS -lt $SECRET_NUMBER ]]
+    then
+      echo "It's higher than that, guess again:"
+    elif [[ $GUESS -gt $SECRET_NUMBER ]]
+    then
+      echo "It's lower than that, guess again:"
+    fi
+  fi
+done
+
+echo "You guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $GUESSES);")
