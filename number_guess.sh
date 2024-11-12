622ea2e4e37bed4fe828a4fa0645a7f78f3ccfb3
Ma. Agustina  Benitez Vanni <magustinabenitez@gmail.com>
2024-11-12 14:33:16 +0000

    feat: add script logic

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo "Enter your username:"
+read USERNAME
+
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME';")
+
+if [[ -z $USER_ID ]]
+  then
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
+    USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME';")
+    echo "Welcome, $USERNAME! It looks like this is your first time here."  
+  else
+    GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id = $USER_ID;")
+    BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id = $USER_ID;")
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+GUESSES=0
+GUESS=0
